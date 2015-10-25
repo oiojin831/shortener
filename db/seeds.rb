71b782123c6d397bce917e525d64a1e11bb59499
eungjinlee <oiojin831@gmail.com>
2015-10-25 05:15:14 +0000

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+80000.times do
+  shorten = Shorten.create(original_url: "https://tumblbug.com/")
+  shorten.update_attribute(:short_url, Base62.encode(shorten.id).rjust(6, '0'))
+end
